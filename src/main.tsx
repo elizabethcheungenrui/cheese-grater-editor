ade1dbac8c37908db8b310ed7319a000649dadf3
Elizabeth Cheung <elizabethcheungenrui@gmail.com>
2026-01-03 06:53:24 +0000

    archive, print progress

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,6 @@ import App from "./pages/App";
 import "./main.css";
 import ScrollToTop from "./methods/ScrollToTop";
 import * as pdfjsLib from "pdfjs-dist";
-import {
-  EventBus,
-  PDFViewer,
-} from "pdfjs-dist/web/pdf_viewer.mjs"
 
 import "pdfjs-dist/web/pdf_viewer.css"
 
